62d181fedc5ae398b4bc3e24b5944f9ffee62d4f
sliva <sliva@DESKTOP-7M27B1J>
2024-05-10 16:26:34 -0400

    Update to SDV 1.6 / SMAPI 4.0

---
+++ ManyEnchantments/ModConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CombineManyRings
+{
+    public sealed class ModConfig
+    {
+        public bool AllowSameRing { get; set; } = false;
+        public bool BalancedMode { get; set; } = false;
+        public bool DestroyRingOnFailure { get; set; } = false;
+        public int FailureChancePerExtraRing { get; set; } = 20;
+        public int CostPerExtraRing { get; set; } = 100;
+    }
+}
